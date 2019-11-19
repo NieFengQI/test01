b5b4c6f53136dc881384ae04125407a9d15a337c
NieFengQI <54741808+NieFengQI@users.noreply.github.com>
2019-11-19 16:59:33 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("ÄãºÃ");
-		System.out.println("ÊÀ½ç");
+		System.out.println("Hello");
+		System.out.println("World");
 	}
 }
